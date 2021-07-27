01106c2e1a6ce8911ffa7a83db160da6d59b5373
nurgitlab <nurbek.math@gmail.com>
2021-07-27 20:44:36 +0600

    fixed problem with bootstrap

--- noteapp/src/App.js
+++ noteapp/src/App.js
@@ -2,10 +2,14 @@ import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom'
 import { About } from "./pages/About";
 import { Home } from "./pages/Home";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+
+      <Navbar />
+
       <div className="container pt-4">
         <Switch>
           <Route path={'/'} exact component={Home}/>
---
+++ noteapp/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+export const Navbar = () => (
+  <nav className="navbar navbar-dark navbar-expand-lg bg-primary">
+    <div className="navbar-brand">
+      Note App
+    </div>
+  </nav>
+)

c9203699502f7479865cd6211cb3b035a1039daa
nurgitlab <nurbek.math@gmail.com>
2021-07-27 21:06:13 +0600

    bug fixed

--- noteapp/src/pages/About.js
+++ noteapp/src/pages/About.js
@@ -1,9 +1,12 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
-export const About = () => {
-  return (
-    <Fragment>
-      <h1>About page</h1>
-    </Fragment>
-  )
-}
+export const About = () => (
+  <div className="jumbotron">
+    <div className="container">
+      <h1 className="display-4">Блокнот на Реакте и на хуках</h1>
+      <p className="lead">
+        Версия приложения <strong>2.2.8</strong>
+      </p>
+    </div>
+  </div>
+)

40fea55cf779412df3f87f11143356d3a18e6919
nurgitlab <nurbek.math@gmail.com>
2021-07-28 01:57:57 +0600

    alert

--- noteapp/src/components/Alert.js
+++ noteapp/src/components/Alert.js
@@ -1,8 +1,13 @@
-import React from 'react'
-export const Alert = ({alert}) => {
+import React, {useContext} from 'react'
+import { AlertContext } from "../context/alert/alertContext";
+
+export const Alert = () => {
+  const {alert,hide}=useContext(AlertContext)
+
   if(!alert){
     return null
   }
+
   return(
     <div className={`alert alert-${alert.type ||'warning'} alert-dismissible`}>
       <strong>
@@ -10,6 +15,7 @@ export const Alert = ({alert}) => {
       </strong>
       {alert.text}
       <button
+        onClick={hide}
         type="button"
         className="close"
         data-dismiss="alert"
--- noteapp/src/components/form.js
+++ noteapp/src/components/form.js
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, {useContext, useState} from 'react'
+import { AlertContext } from "../context/alert/alertContext";
 
 export const Form = () => {
+  const [value, setValue]= useState('')
+  const alert=useContext(AlertContext)
+
+  const submitHandler = event => {
+    event.preventDefault()
+    alert.show(value, 'success')
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="form-group">
         <input
           type="text"
           className="form-control"
           placeholder="Введите название заметки"
+          value={value}
+          onChange={e => setValue(e.target.value)}
+
         />
 
       </div>

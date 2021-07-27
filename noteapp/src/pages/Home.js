246428d3a2d3162534ff451951c308f8adc0428f
nurgitlab <nurbek.math@gmail.com>
2021-07-27 21:33:36 +0600

    navbar style changed

--- noteapp/src/pages/Home.js
+++ noteapp/src/pages/Home.js
@@ -1,9 +1,21 @@
 import React, { Fragment } from 'react';
+import { Form } from "../components/form";
+import { Notes } from "../components/Notes";
 
 export const Home = () => {
+
+  const notes = new Array(3)
+    .fill('')
+    .map((_,i)=>({id:i,title:`Note ${i+1}`}))
+
   return (
     <Fragment>
-      <h1>Home page</h1>
+      <Form />
+
+      <hr />
+
+      <Notes notes={notes}/>
+
     </Fragment>
   )
 }

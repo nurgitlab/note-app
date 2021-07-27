a070b995943f58f8c518cb16704a40c09def6925
nurgitlab <nurbek.math@gmail.com>
2021-07-27 20:19:08 +0600

    changed style of navbar

--- noteapp/src/App.js
+++ noteapp/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
 
 function App() {
   return (
-    <div className="container">
-        <h1>Test</h1>
-    </div>
+    <BrowserRouter>
+      <div className="container pt-4">
+        <Switch>
+          <Route path={'/'} exact component={Home}/>
+          <Route path={'/about'} component={About}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
+
   );
 }
 

839ed817881c5829e8b4d040e8728d465da62fa9
nurgitlab <nurbek.math@gmail.com>
2021-07-28 20:32:29 +0600

    animation for alerts

--- noteapp/src/components/Alert.js
+++ noteapp/src/components/Alert.js
@@ -1,32 +1,40 @@
 import React, {useContext} from 'react'
 import { AlertContext } from "../context/alert/alertContext";
+import { CSSTransition } from 'react-transition-group'
 
 export const Alert = () => {
   const {alert,hide}=useContext(AlertContext)
 
-  if(!alert.visible){
-    return null
-  }
-
   return(
-    <div className={`alert alert-${alert.type ||'warning'} alert-dismissible`}>
-      <strong>
-        Внимание!
-      </strong>
-      &nbsp;{alert.text}
-      <button
-        onClick={hide}
-        type="button"
-        className="close"
-        data-dismiss="alert"
-        aria-label="Close"
-      >
+    <CSSTransition
+      in={alert.visible}
+      timeout={{
+        enter: 500,
+        exit: 350
+      }}
+      classNames={'alert'}
+      mountOnEnter
+      unmountOnExit
+    >
+      <div className={`alert alert-${alert.type ||'warning'} alert-dismissible`}>
+        <strong>
+          Внимание!
+        </strong>
+        &nbsp;{alert.text}
+        <button
+          onClick={hide}
+          type="button"
+          className="close"
+          data-dismiss="alert"
+          aria-label="Close"
+        >
         <span
           aria-hidden="true"
         >
           &times;
         </span>
-      </button>
-    </div>
+        </button>
+      </div>
+    </CSSTransition>
   )
 }
